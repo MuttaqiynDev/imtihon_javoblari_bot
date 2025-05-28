b470495733a86d0fa6289bd13971d9565ade2134
unknown <muttaqiyndev@gmain.com>
2025-05-28 18:25:30 +0500

    claude commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Bot Token - BotFather'dan olingan token
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "7604674249:AAEtcbRNynd42dAwpYE9w11BSV8H9Bq4-W0"
 
 # Ma'lumotlar bazasi fayli
 DATABASE_FILE = "exam_bot.db"
